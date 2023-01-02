c6b42cb876bce2aacaa0e3e87bf2063f5a0c2892
tapper2 <shay@tapper.co.il>
2023-01-02 17:35:43 +0200

    mongoose crud

--- node-with-mongo/src/controllers/courses.ts
+++ node-with-mongo/src/controllers/courses.ts
@@ -1,7 +1,9 @@
 import { Icourse } from '../interfaces/course'
 import { Course } from '../models/course'
-import { Document } from 'mongoose'
+import { Document, Types } from 'mongoose'
+import { ObjectID } from 'typeorm'
 
+//create
 export const createCourse = async (
   doc: Icourse
 ): Promise<Document<unknown, any, Icourse>> => {
@@ -9,6 +11,7 @@ export const createCourse = async (
   return await course.save()
 }
 
+//read/find
 export const findCourseById = async (
   id: string
 ): Promise<Document<unknown, any, Icourse> | null> => {
@@ -42,4 +45,119 @@ export const findCourses = async (filter: {
     default:
       return await Course.find()
   }
+}
+
+export const findCoursesWithLimitSortSelect = async (
+  limit: number,
+  sortBy: string,
+  keys: string[],
+  desc: boolean
+): Promise<Document<unknown, any, Icourse>[]> => {
+  const select: any = {}
+  for (const key of keys) {
+    select[key] = 1
+  }
+  return await Course.find()
+    .limit(limit)
+    .sort({ [sortBy]: desc ? -1 : 1 })
+    .select(select)
+  // keys = ["name", "price"]
+  // select = {
+  //     "name": 1
+  //     "price" : 1
+  // }
+}
+
+export const findCoursesStartWith = async (
+  key: string,
+  str: string
+): Promise<Document<unknown, any, Icourse>[]> => {
+  return await Course.find({ [key]: new RegExp(`^${str}`) })
+}
+
+export const findCoursesByPriceRange = async (
+  gte: number,
+  lte: number
+): Promise<Document<unknown, any, Icourse>[]> => {
+  return await Course.find({
+    price: {
+      $gte: gte,
+      $lte: lte,
+    },
+  })
+}
+
+export const findCoursesOfSpecificLecturers = async (
+  lecturers: string[]
+): Promise<Document<unknown, any, Icourse>[]> => {
+  return await Course.find({
+    lecturer: {
+      $in: lecturers,
+    },
+  })
+}
+
+export const findCoursesBySpecificOrRangePrice = async (
+  eq: number,
+  gt: number,
+  lt: number
+): Promise<Document<unknown, any, Icourse>[]> => {
+  console.log(eq, gt, lt)
+  //   return await Course.find({
+  // $or: [
+  //   { price: eq },
+  //   {
+  //     price: {
+  //       $gt: gt,
+  //       $lt: lt,
+  //     },
+  //   },
+  // ],
+  //   })
+  return await Course.find().or([
+    { price: eq },
+    {
+      price: {
+        $gt: gt,
+        $lt: lt,
+      },
+    },
+  ])
+}
+
+export const findCoursesWithPagination = async (
+  pageNumber: number,
+  pageSize: number
+): Promise<Document<unknown, any, Icourse>[]> => {
+  return await Course.find()
+    .skip((pageNumber - 1) * pageSize)
+    .limit(pageSize)
+}
+
+//update
+
+export const updateCourse = async (
+  id: string,
+  data: Icourse,
+  onlyOnline: boolean,
+  createNew: boolean
+): Promise<Document<unknown, any, Icourse> | null | any> => {
+  if (onlyOnline) {
+    const [course] = await Course.find({ _id: id, isOnline: true })
+    console.log(course)
+    if (!course) return null
+    course.set(data)
+    return await course.save()
+  }
+  return await Course.findByIdAndUpdate(
+    id,
+    { $set: data },
+    { upsert: createNew, new: createNew }
+  )
+}
+
+// delete
+export const deleteCourses = async (topic: string): Promise<number> => {
+  const { deletedCount } = await Course.deleteMany({ topics: topic })
+  return deletedCount 
 }
--- node-with-mongo/src/routes/courses.ts
+++ node-with-mongo/src/routes/courses.ts
@@ -1,40 +1,155 @@
 import { Router, Request, Response } from 'express'
-import { createCourse, findCourseById, findCourses } from '../controllers/courses';
-
+import {
+  createCourse,
+  deleteCourses,
+  findCourseById,
+  findCourses,
+  findCoursesByPriceRange,
+  findCoursesBySpecificOrRangePrice,
+  findCoursesOfSpecificLecturers,
+  findCoursesStartWith,
+  findCoursesWithLimitSortSelect,
+  findCoursesWithPagination,
+  updateCourse,
+} from '../controllers/courses'
 
 const router: Router = Router()
 
 router.post('/', async (req: Request, res: Response) => {
-    try {
-        const course = await createCourse(req.body)
-        res.send(course)
-    } catch (error) {
-        console.error(error)
-        res.sendStatus(500)
-    }
+  try {
+    const course = await createCourse(req.body)
+    res.send(course)
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(500)
+  }
 })
 
+router.get('/price-ranges', async (req: Request, res: Response) => {
+  try {
+    const { gte, lte } = req.query
+    const courses = await findCoursesByPriceRange(
+      +(gte as string),
+      +(lte as string)
+    )
+    console.log(courses)
+    courses.length ? res.send(courses) : res.sendStatus(404)
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(500)
+  }
+})
 
-router.get('/:id', async (req: Request, res: Response) => {
-    try {
-        const course = await findCourseById(req.params.id)
-        course ? res.send(course) : res.sendStatus(404)
-    } catch (error) {
-        console.error(error)
-        res.sendStatus(500)
-    }
+router.get('/price-specific-or-ranges', async (req: Request, res: Response) => {
+  try {
+    const { eq, gt, lt } = req.query
+    console.log('dfjghdfjhfgjbf')
+    console.log(eq, gt, lt)
+    const courses = await findCoursesBySpecificOrRangePrice(
+      +(eq as string),
+      +(gt as string),
+      +(lt as string)
+    )
+    console.log(courses)
+    courses.length ? res.send(courses) : res.sendStatus(404)
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(500)
+  }
 })
 
 router.get('/', async (req: Request, res: Response) => {
-    try {
-        const courses = await findCourses(req.query)
-        courses.length ? res.send(courses) : res.sendStatus(404)
-    } catch (error) {
-        console.error(error)
-        res.sendStatus(500)
-    }
+  try {
+    const courses = await findCourses(req.query)
+    courses.length ? res.send(courses) : res.sendStatus(404)
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(500)
+  }
 })
 
+router.post('/limit-sort-select', async (req: Request, res: Response) => {
+  try {
+    const { limit, sortBy, keys, desc } = req.body
+    const courses = await findCoursesWithLimitSortSelect(
+      limit,
+      sortBy,
+      keys,
+      desc
+    )
+    courses.length ? res.send(courses) : res.sendStatus(404)
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(500)
+  }
+})
+
+router.get('/start-with/:key/:prefix', async (req: Request, res: Response) => {
+  try {
+    const { key, prefix } = req.params
+    const courses = await findCoursesStartWith(key, prefix)
+    courses.length ? res.send(courses) : res.sendStatus(404)
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(500)
+  }
+})
+
+router.get('/pagination', async (req: Request, res: Response) => {
+  try {
+    const { pageNumber, pageSize } = req.query
+    const courses = await findCoursesWithPagination(
+      +(pageNumber as string),
+      +(pageSize as string)
+    )
+    courses.length ? res.send(courses) : res.sendStatus(404)
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(500)
+  }
+})
+
+router.get('/:id', async (req: Request, res: Response) => {
+  try {
+    const course = await findCourseById(req.params.id)
+    course ? res.send(course) : res.sendStatus(404)
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(500)
+  }
+})
+
+router.post('/by-lecturers', async (req: Request, res: Response) => {
+  try {
+    const { lecturers } = req.body
+    const courses = await findCoursesOfSpecificLecturers(lecturers)
+    courses.length ? res.send(courses) : res.sendStatus(404)
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(500)
+  }
+})
+
+router.put('/', async (req: Request, res: Response) => {
+  try {
+    const { id, data, onlyOnline, createNew } = req.body
+    const course = await updateCourse(id, data, onlyOnline, createNew)
+    course ? res.send(course) : res.sendStatus(404)
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(500)
+  }
+})
+
+router.delete('/tag/:tag', async (req: Request, res: Response) => {
+  try {
+    const count = await deleteCourses(req.params.tag)
+    count ? res.send(`${count} record deleted`) : res.sendStatus(404)
+  } catch (error) {
+    console.error(error)
+    res.sendStatus(500)
+  }
+})
 
 export default router
 
